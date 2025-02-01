f388d87f66e6fc1806e958a7219108bb3b794ba6
alefeio@gmail.com <alefeio@gmail.com>
2025-02-01 00:14:45 -0300

    corrigindo erros 6

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
