8746121b0c0fe59d66c0525aeaa6cf0012f24d96
alefeio@gmail.com <alefeio@gmail.com>
2025-02-04 14:20:17 -0300

    dynamic

---
+++ app/[id]/DP.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+
+const DynamicPageBuilder = dynamic(() => import("@/app/[id]/DynamicPage"), {
+    ssr: false,
+});
+
+export default function LandingPage() {
+    return <DynamicPageBuilder />;
+}
---
+++ app/admin/lp/[id]/EP.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+
+// Importa o LandingPageBuilder de forma dinâmica, desativando o SSR
+const EditPageBuilder = dynamic(() => import("@/app/admin/lp/[id]/EditPage"), {
+    ssr: false, // ❌ Impede a execução no servidor, evitando o erro com `window`
+});
+
+export default function LandingPage() {
+    return <EditPageBuilder />;
+}
